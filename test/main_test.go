4f3b111b267dbc741996b2343f194b28a0e883d8
Orkhan Jamaladdinov <orxan.camaleddinov@gmail.com>
2021-05-14 02:56:58 +0300

    CC4-test-added

--- test/main_test.go
+++ test/main_test.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 )
 
 func TestPass(t *testing.T) {
 	fmt.Println("Always passing test")
+}
+
+func TestGetPlantHire(t *testing.T) {
+	resp, err := http.Get("http://localhost:8081/api/plant-hires/1")
+	if err != nil {
+		t.Error("Problem getting single plant hire via REST.")
+		return
+	}
+
+	if resp.Status != "200 OK" {
+		t.Error("Could get plant hire with this id")
+		return
+	}
 }
